ebd06025d80d0c35f9cfc689a0753431b44f0a84
Sfoste18 <stephen.foster1981@outlook.com>
2025-03-04 18:12:31 +0000

    commit

---
+++ SF-gd4-Wk10/Assets/BasicFarmingSimulator/Scripts/Mouse clicks.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mouseclicks : MonoBehaviour
+{
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //agent.SetDestination(hit.point);
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //agent.SetDestination(hit.point);
+            }
+
+        }
+
+
+
+    }
+}
